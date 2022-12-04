9467c044ae679071fd302467294167aa864a8a2d
Nsree1997 <navyasribogi103@gmail.com>
2022-12-04 19:10:27 +0530

    numpy

--- NUMPY/numpy_array.py
+++ NUMPY/numpy_array.py
@@ -5,3 +5,23 @@ arr = np.array([1, 2, 3, 4, 5])
 print(arr)
 
 print(type(arr))
+import numpy as np
+
+arr =np.array((1,2,3,4,5,))
+
+print(arr)
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+import numpy as np 
+
+arr = np.array([1,2,3,][4,5,6])
+
+print(arr)
+import numpy as np
+
+arr = np.array([[[1,2,2],[4.5,6]],[[7,8,9],[8,9,10]]])
+
+print(arr)

ef60ed6abadbf135c036cac923c0b61608914f7a
Nsree1997 <navyasribogi103@gmail.com>
2022-12-09 14:38:37 +0530

    add

---
+++ python_numbers.py
@@ -0,0 +1,20 @@
+#convert from int to float:
+x = float(5)
+
+#convert from float to int:
+y = int(6.5)
+
+#convert from int to complex:
+z = complex(2)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+import random
+
+print(random.randrange(6, 10))

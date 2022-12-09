70ef7ab67af6b570f8fa3a8f52b30230faa28a4a
Nsree1997 <navyasribogi103@gmail.com>
2022-12-09 12:19:23 +0530

    add

---
+++ python_comment.py
@@ -0,0 +1,19 @@
+# this is a comment
+print("hi,navyasree !")
+
+print("hi,navyasree!") # this is a comment
+
+# print("hi,navya!")
+print("hello,navyasree!")
+
+# this is a comment
+# wrriten in
+# more than just one line
+print("hi navyasree!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("hi,navyasre!")

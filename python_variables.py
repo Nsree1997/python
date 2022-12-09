e82eaa39553bb569b698cbd6d20402871f1302ae
Nsree1997 <navyasribogi103@gmail.com>
2022-12-09 14:12:17 +0530

    add

---
+++ python_variables.py
@@ -0,0 +1,77 @@
+x = 4
+x = "navya sree"
+print(x)
+
+x = 3              # x is of int type 
+x = "navyasree"    # x is now str type
+print(x)
+
+x = str(3)   # x will be '3'
+y = int(3)   # y will be 3
+z = float(3) # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "navyasre"
+print(type(x))
+print(type(y))
+
+x = 'navyasree'
+# is the same as
+x = "navyasree"
+
+a = 3
+A = "navya sree"
+print(a)
+print(A)
+
+myvar = "navya sree"
+my_var = "navya sree"
+_my_var = "navya sree"
+myVar = "navya sree"
+MYVAR = "navya sree"
+myvar2 = "navya sree"
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+x,y,z = "red","yellow","white"
+print(x)
+print(y)
+print(z)
+
+fruits = ["orange","apple","cherry"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+x = "world"
+
+def myfunc():
+    print("python is" + x)
+
+myfunc()
+
+x = "awesome"
+
+def myfunc():
+    x = "world"
+    print("print is "+ x) 
+
+myfunc() 
+
+print("print is" + x)
+
